da5e8b79b141ebadcfcd83d1a5a98bb4c1fde821
Gianluca Mazzini <gianluca@mazzini.org>
2025-02-04 06:48:16 +0100

    Update rotate.php

--- rotate.php
+++ rotate.php
@@ -1,13 +1,13 @@
 <?php
 include("setup.php");
 
-for(;;){
+
   $photo=scandir($base."img",SCANDIR_SORT_DESCENDING);
   $act=file_get_contents($base."next");
   $key=array_search($act,$photo);
   $len=count($photo);
   if(++$key>=$len-2)$key=0;
   file_put_contents("next",$photo[$key]);
-}
+
 
 ?>
