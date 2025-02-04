84a2b51be2b9a357fa29db8b7379b86e7be23f3a
Gianluca Mazzini <gianluca@mazzini.org>
2025-02-04 06:16:05 +0100

    Create index.php

---
+++ index.php
@@ -0,0 +1,22 @@
+<?php
+$base="/home/www/corteconnessa/";
+$act=file_get_contents($base."next");
+?>
+
+<head>
+<meta http-equiv='refresh' content='6' />
+<title>Corte Connessa</title>
+<style>
+  body {background-color: #F9F4B7;}
+</style>
+</head>
+
+<body>
+<center>
+<a href='https://www.corteconnessa.it/up.php'>Upload</a>
+&nbsp; &nbsp; &nbsp;
+Nome Foto: <?php echo $act; ?>
+<br>
+</center>
+<img src='img/<?php echo $act ?>' style='width: 100%; height: 100%; object-fit: contain;'>
+</body>
